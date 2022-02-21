e1ec84f7a076402c127e9a05ec347b5094c68605
Dwight Mckenzie <94255952+DwightMckenzie@users.noreply.github.com>
2022-02-21 17:28:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,30 +4,26 @@ const ftr = document.getElementById('ftr');
 const mnthArr = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 const dyArr = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
 
-// this "function" gets the "var" request and over writes td data.
 function currentTime() {
 	const d = new Date();
 	let hr = d.getHours();
 	let min = d.getMinutes();
 	let ampm = 'AM';
-	let dy = dyArr[d.getDay()]; // will need to rig array
+	let dy = dyArr[d.getDay()];
 	let dt = d.getDate();
-	let mnth = mnthArr[d.getMonth()]; // will need to rig array
+	let mnth = mnthArr[d.getMonth()];
 	let yr = d.getFullYear();
 	let msg = '';
 
-	// hr greater than 12
 	if (hr >= 12) {
 		hr = hr - 12;
 		ampm = 'PM';
 	}
 
-	// min less than 10
 	if (min < 10) {
 		min = `0${min}`;
 	}
 
-	// message according to time
 	if (hr < 6 && ampm === 'AM') {
 		msg = "Don't disturb!, Sleeping!.";
 	} else if (hr === 7 && ampm === 'AM') {
@@ -46,19 +42,14 @@ function currentTime() {
 		msg = 'Nothing at this time!';
 	}
 
-	// gets battery level
 	navigator.getBattery().then(function (e) {
-		// console.log(bttry.level);
 		let bttry = parseInt(e.level * 100);
 		ftr.children[1].innerHTML = `<i class="fa fa-battery-half" aria-hidden="true"></i> ${bttry}%`;
 	});
 
-	// prints results
 	hdr.children[0].textContent = `${msg}`;
 	bdy.children[0].textContent = `${hr}:${min} ${ampm}`;
 	ftr.children[0].textContent = `${dy} - ${mnth}/${dt}/${yr}`;
 }
 
-// refire function
-// var refresh = setInterval(currentTime, 1000);
-currentTime();
+var refresh = setInterval(currentTime, 1000);
